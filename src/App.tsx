930f672e4c51461ca309adbe0f470d757dab8a68
NamikoToriyama <torichan@mercari.com>
2024-01-15 18:30:59 +0900

    add step4

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,39 @@ import detectEthereumProvider from '@metamask/detect-provider'
 
 const App = () => {
   const [hasProvider, setHasProvider] = useState<boolean | null>(null)
+  const initialState = { accounts: [] }               /* New */
+  const [wallet, setWallet] = useState(initialState)  /* New */
 
   useEffect(() => {
     const getProvider = async () => {
       const provider = await detectEthereumProvider({ silent: true })
-      console.log(provider)
-      setHasProvider(Boolean(provider)) // transform provider to true or false
+      setHasProvider(Boolean(provider))
     }
 
     getProvider()
   }, [])
 
+  const updateWallet = async (accounts:any) => {     /* New */
+    setWallet({ accounts })                          /* New */
+  }                                                  /* New */
+
+  const handleConnect = async () => {                /* New */
+    let accounts = await window.ethereum.request({   /* New */
+      method: "eth_requestAccounts",                 /* New */
+    })                                               /* New */
+    updateWallet(accounts)                           /* New */
+  }                                                  /* New */
+
   return (
     <div className="App">
       <div>Injected Provider {hasProvider ? 'DOES' : 'DOES NOT'} Exist</div>
-      { hasProvider &&
-        <button>Connect MetaMask</button>
+
+      { hasProvider &&                               /* Updated */
+        <button onClick={handleConnect}>Connect MetaMask</button>
+      }
+      
+      { wallet.accounts.length > 0 &&                /* New */
+        <div>Wallet Accounts: { wallet.accounts[0] }</div>
       }
     </div>
   )
